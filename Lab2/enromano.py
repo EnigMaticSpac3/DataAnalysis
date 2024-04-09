b97f91a59dbb1a130d65f1f4149a655b18c049cf
enigmaticspac3 <jgonzreal62@gmail.com>
2024-04-09 09:59:21 -0500

    programa4

---
+++ Lab2/enromano.py
@@ -0,0 +1,17 @@
+# Este programa convierte un número entero a su representación en números romanos.
+
+def enromano():
+    numero = int(input("Ingrese un número entre 1 y 1000: "))
+    if numero < 1 or numero > 1000:
+        print("Número fuera de rango")
+    else:
+        romanos = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD',
+                   500: 'D', 900: 'CM', 1000: 'M'}
+        resultado = ''
+        for valor, letra in sorted(romanos.items(), reverse=True):
+            while numero >= valor:
+                resultado += letra
+                numero -= valor
+        print(resultado)
+
+enromano()
